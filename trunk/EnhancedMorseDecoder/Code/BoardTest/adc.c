0603e46ec15bc9c5fd42e22cf3453dfc1f5cdd61
dthorner1 <dthorner1@4fe16c79-47b3-ea85-171c-90f8eb6687ea>
2011-03-29 00:00:44 +0000

    ADC code in progress of being modified

--- trunk/EnhancedMorseDecoder/Code/BoardTest/adc.c
+++ trunk/EnhancedMorseDecoder/Code/BoardTest/adc.c
@@ -31,30 +31,33 @@ REFRENCES:
  *****************************************************************************/
 void init_adc(void){
 	
-	AD1CON1bits.FORM   = 3;		// Data Output Format: Signed Fraction (Q15 format)
-	AD1CON1bits.SSRC   = 2;		// Sample Clock Source: GP Timer starts conversion - TMR3
-	AD1CON1bits.ASAM   = 1;		// ADC Sample Control: Sampling begins immediately after conversion
-	AD1CON1bits.AD12B  = 1;		// 12-bit ADC operation
+	AD1CON1bits.AD12B	= 1;		// 12-bit ADC operation
+	AD1CON2bits.VCFG	= 0;		// Select VrefH = AVdd and VrefL - AVss
+	
+	AD1CON1bits.FORM	= 3;		// Data Output Format: Signed Fraction (Q15 format)
+	AD1CON1bits.SSRC	= 2;		// Sample Clock Source: GP Timer starts conversion - TMR3
+	AD1CON1bits.ASAM	= 1;		// ADC Sample Control: Sampling begins immediately after conversion
+	
 
-	AD1CON2bits.CHPS  = 0;		// Converts CH0
+	AD1CON2bits.CHPS	= 0;		// Converts CH0
      
-	AD1CON3bits.ADRC=0;			// ADC Clock is derived from Systems Clock (ADC_CHANNEL_FCY)
-	AD1CON3bits.ADCS = 63;		// ADC Conversion Clock Tad=Tcy*(ADCS+1)= (1/40M)*64 = 1.6us (625Khz)
-								// ADC Conversion Time for 12-bit Tc=14*Tad = 22.4us 
-								// slowest converstion speeed possible. 0x3F(63) means divide by 64
+	AD1CON3bits.ADRC	= 0;		// ADC Clock is derived from Systems Clock (ADC_CHANNEL_FCY)
+	AD1CON3bits.ADCS	= 63;		// ADC Conversion Clock Tad=Tcy*(ADCS+1)= (1/40M)*64 = 1.6us (625Khz)
+									// ADC Conversion Time for 12-bit Tc=14*Tad = 22.4us 
+									// slowest converstion speeed possible. 0x3F(63) means divide by 64
 					
-	AD1CON1bits.ADDMABM = 1; 	// DMA buffers are built in conversion order mode ?
-	AD1CON2bits.SMPI    = 0;	// SMPI must be 0. Generates Interrupt after completion of 
-								// every sample/conversion operation
+	AD1CON1bits.ADDMABM	= 1; 		// DMA buffers are built in conversion order mode ?
+	AD1CON2bits.SMPI	= 0;		// SMPI must be 0. Generates Interrupt after completion of 
+									// every sample/conversion operation
 
     //AD1CHS0: A/D Input Select Register
-    AD1CHS0bits.CH0SA=0;		// MUXA +ve input selection (AN0) for CH0
-	AD1CHS0bits.CH0NA=0;		// MUXA -ve input selection (Vref-) for CH0
+    AD1CHS0bits.CH0SA	= 0;		// MUXA +ve input selection (AN0) for CH0
+	AD1CHS0bits.CH0NA	= 0;		// MUXA -ve input selection (Vref-) for CH0
 
     //AD1PCFGH/AD1PCFGL: Port Configuration Register
-	AD1PCFGL=0xFFFF;			// disable all analog inputs
-	AD1PCFGH=0xFFFF;			// disable all analog inputs
-    AD1PCFGLbits.PCFG0 = 0;		// set AN0 as Analog Input
+	AD1PCFGL			= 0xFFFF;	// disable all analog inputs
+	AD1PCFGH			= 0xFFFF;	// disable all analog inputs
+    AD1PCFGLbits.PCFG0 	= 0;		// set AN0 as Analog Input
        
     IFS0bits.AD1IF = 0;			// Clear the A/D interrupt flag bit
     IEC0bits.AD1IE = 0;			// Do Not Enable A/D interrupt  !! This should be enabled eventually
@@ -65,5 +68,4 @@ void init_adc(void){
 
 	IEC0bits.AD1IE = 1;			// Enable A/D interrupt, 
 }	
-
 
