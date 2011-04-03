034c2866d289df6bcd67ff043bb3354f0e217911
dthorner1 <dthorner1@4fe16c79-47b3-ea85-171c-90f8eb6687ea>
2011-04-03 19:04:09 +0000

    After working with Sean in EB2A, code almost complete.

--- trunk/EnhancedMorseDecoder/Code/EnhancedMorseDecoder/decoder_dsp.c
+++ trunk/EnhancedMorseDecoder/Code/EnhancedMorseDecoder/decoder_dsp.c
@@ -10,23 +10,39 @@
 #include "dsp.h"
 #include "math.h"
 
-#define WINDOW_FILTER_SIZE 16
+#define WINDOW_FILTER_SIZE 32
 
 static fractional sin_vals_frac[NUM_PTS];
 static fractional cos_vals_frac[NUM_PTS];
 
+/******************************************************************************
+ * Name: filter_init
+ * Inputs: FIRStruct * filterR - pointer to real filter struct
+ *		   FIRStruct * filterI - pointer to imaginary filter struct
+ * Outputs: n/a 
+ *
+ * Description: Initializes both input filter structs and their delay lines
+ *****************************************************************************/
+void filter_init(FIRStruct * filterR, FIRStruct * filterI){
 
-void filter_init(FIRStruct * pFilter){
-
-	// Initialize FIR Filter   	
-    FIRStructInit( 	pFilter,
-					NCOEFFS,  // num of coeffs
-					filter86_coeffs ,// coefficients
-					COEFF_PAGE_NUM  ,// 	
-					z   // delay
+	// Initialize real FIR Filter   	
+    FIRStructInit( 	filterR,			//
+					NCOEFFS,  			// num of coeffs
+					filter86_coeffs ,	// coefficients
+					COEFF_PAGE_NUM  ,	// 	
+					zR   				// delay for reals
+					);
+	
+	// Initialize imaginary FIR Filter   	
+    FIRStructInit( 	filterI,			//
+					NCOEFFS,  			// num of coeffs
+					filter86_coeffs ,	// coefficients
+					COEFF_PAGE_NUM  ,	// 	
+					zI   				// delay for reals
 					);
    	
-	FIRDelayInit(pFilter);  // initialize the delay line 
+	FIRDelayInit(filterR);  // initialize the delay line 
+	FIRDelayInit(filterI);  // initialize the delay line 
 
 	// These values are defined for Fsig = 700Hz and Fsample = 2100Hz ONLY.
 	//  if you change either of these frequencies then these values must be recomputed.
@@ -35,7 +51,7 @@ void filter_init(FIRStruct * pFilter){
 	sin_vals_frac[2] = Float2Fract(-0.866025403784438);
 	cos_vals_frac[0] = Float2Fract(1.000000000000000);
 	cos_vals_frac[1] = Float2Fract(-0.499999999999999);
-	cos_vals_frac[0] = Float2Fract(-0.500000000000001);
+	cos_vals_frac[2] = Float2Fract(-0.500000000000001);
 
 }
 
@@ -48,7 +64,7 @@ void filter_init(FIRStruct * pFilter){
  * Description: Implements the DSP routine defined in 
  *              Morse Decoder - Whitepaper.docx
  *****************************************************************************/
-fractional decoder_dsp(fractional sample, FIRStruct * pFilter){
+float decoder_dsp(fractional sample, FIRStruct * filterR, FIRStruct * filterI){
 
 	// Variables for Sin/Cos multiply
 	static int index = 0; // index into the sin_vals and cos_vals arrays
@@ -65,31 +81,36 @@ fractional decoder_dsp(fractional sample, FIRStruct * pFilter){
   	static int   window_idx;
   	static float average = 0;
   	static float debug_fsigR, debug_fsigI;
-
+  	
+  	
+  	
 	///////////////////////////////////////////////////////
 	// Multiply by sine and cosine                       //
 	///////////////////////////////////////////////////////
 
-	sigR = sample * cos_vals_frac[index]; // real output
-	sigI = sample * sin_vals_frac[index]; // imaginary output
-
+	// the "*" sign cannot be used because fracts are seen as integers by the compiler
+	VectorMultiply(1, &sigR, &sample, &cos_vals_frac[index]); // real output
+	VectorMultiply(1, &sigI, &sample, &sin_vals_frac[index]); // imaginary output
+	
 	index = index + 1;
 	// if the pointer has reached the end of the array
 	if(index == NUM_PTS){
 		index = 0; // reassign to the begining
 	}
 	
+	
 	///////////////////////////////////////////////////////
 	// Lowpass filter                                    //
 	///////////////////////////////////////////////////////
 	// number samples, output, input, filter struct
-	FIR(1,&fsigR,&sigR,pFilter);  // Real
-	FIR(1,&fsigI,&sigI,pFilter);  // Imag	
+	FIR(1,&fsigR,&sigR,filterR);  // Real
+	FIR(1,&fsigI,&sigI,filterI);  // Imag	
 	
 	//convert results to float so we can use math.h functions :-)
 	debug_fsigR = Fract2Float(fsigR);
 	debug_fsigI = Fract2Float(fsigI);
 	mag = 	sqrtf( powf(debug_fsigR,2) + powf(debug_fsigI,2));
+	
 	
 	///////////////////////////////////////////////////////
 	// Window Avg Filter                                 //
--- trunk/EnhancedMorseDecoder/Code/EnhancedMorseDecoder/main.c
+++ trunk/EnhancedMorseDecoder/Code/EnhancedMorseDecoder/main.c
@@ -8,13 +8,9 @@ CHANGE HISTORY:
 	2: 04/01/2011 - Dylan Thorner - ADC testing 
 -------------------------------------------------------------------------------------------------
 DESCRIPTION:
-	Main file for the project to test the dsPIC33FJ128GP802 microcontroller on the Enhanced 
+	Main file for the project for the dsPIC33FJ128GP802 microcontroller on the Enhanced 
 	Morse Decoder Board Rev. 1.0.
-	
-	This program provides the following test outputs:
-		LED 2 is lit when voltage on ADC exceeds 1.65 V (check LED and ADC configuration)
-		LED 1 is toogleing at 1050Hz (to check LED, oscillator, and Fs)
-		ADC is converting from pin 2 and storeing samples in adc_vals (check ADC operation)
+
 ------------------------------------------------------------------------------------------------*/
 
 #include "p33fxxxx.h" 	// includes correct header for processor
@@ -28,15 +24,17 @@ DESCRIPTION:
 
 
 #define FLASH_LED 		// Define this to have LED 1 flash and LED 2 on, else comment out
-#define DEBUG_MODE 1	// Define this to create a sinusiod rather that get sames from ADC
+//#define DEBUG_MODE 1	// Define this to create a sinusiod rather that get sames from ADC
 						//select Debugger --> Select Tool --> MPLAB SIM
 
 //////////////////////
 // Global Variables //
 //////////////////////
-volatile FIRStruct pFilter; 	// struct used by the FIR Filter function
-volatile fractional proc_samp;	// the sample after it has been processed by the DSP routine
-
+FIRStruct filterI; 	// struct used by the FIR Filter function for real data
+FIRStruct filterR; 	// struct used by the FIR Filter function for img data
+volatile float proc_samp;	// the sample after it has been processed by the DSP routine
+volatile float prev_proc_samp [500]; // past processed samples for debugging
+static const float detect_thres = 0.0012; // threshold for detectiong Morse code
 
 
 //////////////////////////
@@ -63,10 +61,11 @@ int main (void){
 	init_leds();			// initialize the LEDs 
 	init_gpio();			// initialize the GPIO pin
 	init_adc();				// initialize the ADC
-	filter_init(&pFilter);	// initialize low pass filter
+	filter_init(&filterR, &filterI);	// initialize low pass filters
+	
 	
 	// Turn on LED2 if FLASH_LED is defined
-	#ifdef FLASH_LED 
+	#ifdef FLASH_LED
 		LED2 = LED_ON;
 	#endif //FLASH_LED
 	
@@ -95,27 +94,44 @@ _ADC1Interrupt():ADC Conversion is complete
  *****************************************************************************/
 void __attribute__((interrupt, no_auto_psv)) _ADC1Interrupt(void){
 	
-	fractional sample; // sample
-	fractional sample_frac;
-	static int sample_cnt = 0;
+	fractional sample; // sample from ADC
+	static int index = 0; // index into proc samp
 
 	// get value from ADC register - ADC1BUF0
     // this assumes that the ADC is configured to output fractionl type
 	sample = ADC1BUF0;
 
-#ifdef DEBUG_MODE
-	//700/2100 == 1/3 == 0.33333
-	sample_frac= Float2Fract(sinf((float)2.0943951*(float)sample_cnt));
-	// 2*3.14159265*((float)(700/2100))* ==> 2.0943951
-	sample_cnt = sample_cnt + 1;
-	proc_samp = decoder_dsp(sample_frac, &pFilter);  //simulation only - building up code by simulating 
-#else	
 	// call the DSP routine with the sample
-    proc_samp = decoder_dsp(sample, &pFilter);
-#endif
-
-	//TODO: 
-	//SquareMagnitudeCplx
+    proc_samp = decoder_dsp(sample, &filterR, &filterI);
+    
+    
+    // for debug: add processed sample to past values array
+    prev_proc_samp[index] = proc_samp;
+    
+    index++;
+    if (index == 500){
+	    index = 0;
+	}
+	
+	// If morse code is detected
+	if(proc_samp > detect_thres){
+		CODE_OUTPUT = DETECTED;
+		
+		// #ifdef
+		LED1 = LED_ON;
+		
+	}
+	// Morse code not detected
+	else{
+		CODE_OUTPUT = NOT_DETECTED;
+		
+		LED1 = LED_OFF;		
+	}	
+	
+		
+	// For sampling rate testing, toggle LED1 each sample
+	//LED1 = LED1 ^ 1; 
+    
 
     IFS0bits.AD1IF = 0;			// Clear the A/D interrupt flag bit
 }
