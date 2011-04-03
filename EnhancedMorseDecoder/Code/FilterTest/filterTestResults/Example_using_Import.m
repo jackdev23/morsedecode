a88d422a10c64b6eb56d9ecfc5d7bf48be8c7916
winfree.sean <winfree.sean@4fe16c79-47b3-ea85-171c-90f8eb6687ea>
2011-04-03 16:47:20 +0000

    added testing for filter

---
+++ EnhancedMorseDecoder/Code/FilterTest/filterTestResults/Example_using_Import.m
@@ -0,0 +1,10 @@
+importfile('fsig_150.mch')
+importfile('fsamp_150.mch')
+figure; plot(fsamp_150)
+hold on; plot(fsig_150,'r')
+
+
+importfile('fsamp_110.mch')
+importfile('fsig_110.mch')
+figure; plot(fsamp_110)
+hold on; plot(fsig_110,'r')
